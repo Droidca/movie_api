e13009d786bb37dcec3e9b921b532f695eed1613
Droidca <david-agostinho@hotmail.com>
2022-03-15 18:01:39 +0000

    Update index.js

--- index.js
+++ index.js
@@ -28,16 +28,7 @@ app.use(morgan('common'));
 
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com', 'http://localhost:1234'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 
 // imports the auth.js file into the project
 // the (app) argument ensures that Express is availible in auth.js file as well
